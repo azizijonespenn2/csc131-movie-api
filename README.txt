5f8495ce89375e1542cf757fec1d73b32240c8cb
Maksim Shaynyuk <maksimshaynyuk@gmail.com>
2020-03-16 17:24:43 -0700

    new data, created route

---
+++ README.txt
@@ -0,0 +1,39 @@
+Required param type for movie api call:
+
+1 - type:
+        - year
+        - category
+        - winner
+
+2 - value:
+        - for type year:
+            - 1993
+            - through
+            - 2017
+        - for type category:
+            - "ACTRESS IN A LEADING ROLE"
+            - "ACTOR"
+            - "ACTRESS"
+            - "ART DIRECTION"
+            - "CINEMATOGRAPHY"
+            - "DIRECTING"
+            - "ENGINEERING EFFECTS"
+            - "UNIQUE AND ARTISTIC PICTURE"
+            - "WRITING (Adaptation)"
+            - "WRITING (Original Story)"
+            - "WRITING (Title Writing)"
+            - "SPECIAL AWARD"
+            - "OUTSTANDING PRODUCTION"
+            - "SOUND RECORDING"
+            - "WRITING"
+            - "SCIENTIFIC OR TECHNICAL AWARD (Class I)"
+            - "SCIENTIFIC OR TECHNICAL AWARD (Class II)"
+            - "SCIENTIFIC OR TECHNICAL AWARD (Class III)"
+            - "SHORT SUBJECT (Cartoon)"
+            - "SHORT SUBJECT (Comedy)"
+            - "SHORT SUBJECT (Novelty)"
+            - "ASSISTANT DIRECTOR"
+            - etc
+        - for type winner:
+            - FALSE
+            - TRUE
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import json
 import os
+import csv
 
 
 # init app
@@ -10,7 +12,54 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get():
-    return jsonify({"Message": "Hello World."})
+    return jsonify({"Message": "Hello World!"})
+
+
+@app.route('/movies', methods=['GET'])
+def getmovies():
+    param = str(request.json['type'])
+    input = str(request.json['value'])
+    if param not in ['year', 'category', 'winner']:
+        return jsonify({"Message": "Bad Request"}), 400
+    results = filereader(param, input)
+    if not results:
+        return jsonify({"Message": "Nothing found"}), 400
+    else:
+        return jsonify(results), 200
+
+
+def filereader(param, input):
+    results = []
+
+    if param == "year":
+        if 1993 > int(input) > 2017:
+            return []
+        with open('data/owd.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                for field in line:
+                    if field == input:
+                        results.append({"year": line[0], "category": line[1], "winner": line[2], "entity": line[3]})
+    elif param == "category":
+        with open('data/owd.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                for field in line:
+                    if field == input:
+                        results.append({"year": line[0], "category": line[1], "winner": line[2], "entity": line[3]})
+    elif param == "winner":
+        if input != "TRUE" and input != "FALSE":
+            return []
+        with open('data/owd.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                for field in line:
+                    if field == input:
+                        results.append({"year": line[0], "category": line[1], "winner": line[2], "entity": line[3]})
+    else:
+        return []
+
+    return results
 
 
 # run server
